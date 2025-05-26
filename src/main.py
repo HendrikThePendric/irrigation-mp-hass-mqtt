e6116474c069a874e921e3c738d829dd313d5025
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2025-05-27 00:37:08 +0200

    chore: use alll the new stuff in main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
 from time import sleep
+import ntptime
+from logger import Logger
 from config import Config
+from wifi import connect_to_wifi
 
+sleep(2)
+
+logger = Logger()
 config = Config("./config.json")
 
+logger.log(str(config))
+connect_to_wifi(config.network, logger)
+ntptime.settime()
+
+counter = 0
+
 while True:
-    print("Hello world")
-    sleep(10)
+    counter += 1
+    msg = f"B {counter}"
+    print(msg)
+    logger.log(msg)
+    sleep(0.005)
