0ae1765105aa9206033e0d471c8a5b4529144d55
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2025-06-16 21:45:00 +0200

    chore: implement test setup with leds and rotary encoders

--- src/irrigation_station.py
+++ src/irrigation_station.py
@@ -1,8 +1,8 @@
+from machine import ADC, Pin
 from config import Config, IrrigationPointConfig
 
 
 from logger import Logger
-
 
 
 class IrrigationPoint:
@@ -13,14 +13,15 @@ class IrrigationPoint:
         """Initialize an irrigation point with its configuration."""
         self.config = config
         self._valve_state: str = IrrigationPoint.STATE_CLOSED
+        self._sensor = ADC(config.sensor_pin)
+        self._valve = Pin(config.valve_pin, Pin.OUT)
         self._sensor_value = 0.5
         self._logger = logger
 
     def get_sensor_value(self) -> float:
         """Simulate and return the current soil moisture sensor value (0.0-1.0)."""
-        self._sensor_value += 0.1
-        if self._sensor_value > 1:
-            self._sensor_value = 0
+        conversion_factor = 1 / (65535)
+        self._sensor_value = self._sensor.read_u16() * conversion_factor
         self._logger.log(
             f"[Sensor] {self.config.name}: Moisture level is {self._sensor_value * 100:.1f}%"
         )
@@ -28,13 +29,19 @@ class IrrigationPoint:
 
     def open_valve(self) -> None:
         """Open the irrigation valve for this point."""
+        self._valve.value(1)
         self._valve_state = IrrigationPoint.STATE_OPEN
-        self._logger.log(f"[Valve] {self.config.name}: Valve opened.")
+        self._logger.log(
+            f"[Valve] {self.config.name}: Valve opened, sent value 1 to pin {str(self.config.valve_pin)}"
+        )
 
     def close_valve(self) -> None:
         """Close the irrigation valve for this point."""
+        self._valve.value(0)
         self._valve_state = IrrigationPoint.STATE_CLOSED
-        self._logger.log(f"[Valve] {self.config.name}: Valve closed.")
+        self._logger.log(
+            f"[Valve] {self.config.name}: Valve closed, sent value 0 to pin {str(self.config.valve_pin)}"
+        )
 
     def get_valve_state(self) -> str:
         """Return the current state (open/closed) of the valve."""
