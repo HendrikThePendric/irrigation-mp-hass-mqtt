0f2fa607736e7a4ead8b52ad3faa78d1194e02cd
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2025-10-28 00:22:20 +0100

    chore: add watchdog class and use it

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from machine import reset, Pin
-from time import sleep, ticks_ms
+from time import sleep
 from mqtt_hass_manager import MqttHassManager
 from irrigation_station import IrrigationStation
 from logger import Logger
+from watchdog import WatchDog
 from config import Config
 from time_keeper import TimeKeeper
 from wifi_manager import WiFiManager
@@ -17,34 +18,37 @@ if PRINT_LOGS:
     # to be visible
     sleep(2)
 
+
 def main() -> None:
     # Initialize all components
     logger = Logger(PRINT_LOGS)
+    watchdog = WatchDog(120, logger)
     time_keeper = TimeKeeper(logger)
     config = Config("./config.json")
     station = IrrigationStation(config, logger)
     wifi_manager = WiFiManager(config.network, logger)
     mqtt_manager = MqttHassManager(config, logger, station)
-    
+
     # Setup components
     logger.log(str(config))
     wifi_manager.setup()
     time_keeper.initialize_ntp_synchronization()
     logger.enable_timestamp_prefix(time_keeper.get_current_cet_datetime_str)
     mqtt_manager.setup()
-    
+
     # LED for visual feedback
     onboard_led = Pin("LED", Pin.OUT)
 
     loop_count = 0
-    
+
     try:
         while True:
+            watchdog.feed()
             wifi_manager.handle_pending_connection_check()
             mqtt_manager.check_msg()
             time_keeper.handle_pending_ntp_sync()
             mqtt_manager.handle_pending_messages()
-            
+
             loop_count += 1
 
             # Run garbage collection every 30 loops (30 seconds) to prevent memory buildup
